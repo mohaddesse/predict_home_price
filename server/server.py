5ba3b855cf2a6f58df13ce2abccbcebd04934b77
mohaddesse <mohaddesseaghasizade@gamil.com>
2022-12-08 19:21:04 +0330

    final backend and frontend

--- server/server.py
+++ server/server.py
@@ -8,7 +8,31 @@ def get_locations_name():
         
         locations=util.get_locations_name()
     )
-    response.header.add("'Access-Control-Allow-Origin', '*'")
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route("/predict",methods=["POST","GET"])
+def predict_price_home():
+    if request.method=="POST":
+        total_sqft = float(request.form['total_sqft'])
+        location = request.form['location']
+        room = int(request.form['room'])
+        bath = int(request.form['bath'])
+
+        response = jsonify({
+            'estimated_price': util.predict_price(location,total_sqft,bath,room)
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    else:
+        return render_template("index.html")
+
+    
+
+@app.route("/home/")
+def home():
+    return render_template("index.html")
 
 if __name__=="__main__":
+    util.load_saved_artifacts()
     app.run(debug=True)
--- server/util.py
+++ server/util.py
@@ -1,3 +1,43 @@
 import json
+import pickle
+import numpy as np
+__locations = None
+__data_columns = None
+__model = None
 
-___
+def predict_price(location,sqft,bath,room):    
+    try:
+        loc_index =__data_columns.index(location.lower())
+    except:
+        loc_index=-1
+        
+    X = np.zeros(len(__data_columns))
+    X[0] = sqft
+    X[1] = bath
+    X[2] = room
+    if loc_index >= 0:
+        X[loc_index] = 1
+    return __model.predict([X])[0]
+
+
+
+
+def load_saved_artifacts():
+    print("load saved arifacts...start")
+    global __data_columns
+    global __model
+    global __locations
+
+    with open("./artifacts/columns.json","r") as f:
+        __data_columns = json.load(f)["data_columns"]
+        __locations=__data_columns[3:]
+        
+    with open("./artifacts/banglore_home_prices_model.pickle","rb") as f:
+        __model=pickle.load(f)
+
+def get_locations_name():
+    return __locations
+
+if __name__ == "__main__":
+    load_saved_artifacts()
+    print(get_locations_name())
